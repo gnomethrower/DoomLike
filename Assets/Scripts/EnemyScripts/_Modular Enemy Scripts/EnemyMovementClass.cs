9ec9dfe4c4afa51f0fa712588019f5730213a06a
gnomethrower <robrobusa@yahoo.de>
2022-08-18 22:22:15 +0200

    Implementing Easy Patrol Movement

--- Assets/Scripts/EnemyScripts/_Modular Enemy Scripts/EnemyMovementClass.cs
+++ Assets/Scripts/EnemyScripts/_Modular Enemy Scripts/EnemyMovementClass.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,7 @@ public class EnemyMovementClass : MonoBehaviour
     // Defining the transform of the object this script is attached to.
     Transform enemy;
     Vector3 spawnPos;
+    LayerMask groundLayer;
 
     // Public Movement Variables
     public float moveSpeed;
@@ -22,6 +23,16 @@ public class EnemyMovementClass : MonoBehaviour
         enemy = gameObject.transform;
         //Defining the current location as the spawnPosition. We will need this for Patrol().
         spawnPos = transform.position;
+        groundLayer = LayerMask.GetMask("Ground");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            Debug.Log("Enter is pressed");
+            PatrolMovement(10f, 5f, 5f, groundLayer);
+        }
     }
 
     public void NoMove()
@@ -43,7 +54,7 @@ public class EnemyMovementClass : MonoBehaviour
         // The movement needed to close distance to the enemy player, according to the meleeRange.
     }
 
-    public void PatrolMovement(float patrolRange, float listenRange, float aggroRange)
+    public void PatrolMovement(float patrolRange, float listenRange, float aggroRange, LayerMask layermaskCollider)
     {
         /*
          * Simple Patrolling behaviour, where entity patrols around their spawn spot, "searching" for the player.
@@ -63,5 +74,32 @@ public class EnemyMovementClass : MonoBehaviour
          * 5. Restart.
          * 
          */
+        Vector3 startingPoint = spawnPos;
+
+
+        //Getting a new point by using a random number between -patrolRange and patrolRange for x and z of the new patrolPoint. for Y, we declare the same height as current position.
+        float x = startingPoint.x + Random.Range(-patrolRange, patrolRange);
+        float z = startingPoint.z + Random.Range(-patrolRange, patrolRange);
+        Vector3 patrolPoint = new Vector3(x, enemy.position.y, z);
+        Debug.Log(patrolPoint);
+
+        /** A short excursion into vector calculation!
+        *  1) This is the formula of calculating the distance between two points:PQ→=(Xq−Xp,Yq−Yp,Zq−Zp)
+        *  2) If we calculate the magnitude/length of the difference, we get the distance between the two vectors.
+        *  3) If we normalize the length, we get the direction.  
+        */
+
+        // 1) Calculating distance between startingPoint and patrolPoint:
+
+        Vector3 distance = patrolPoint - startingPoint;
+        Vector3 direction = distance.normalized;
+        RaycastHit hit;
+        if (Physics.Raycast(startingPoint, direction, out hit, patrolRange, layermaskCollider))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        Debug.DrawLine(spawnPos, patrolPoint, Color.red, 5f);
+
+        // TODO: If a wall is hit, Calculate a length of 2f back towards the startingpoint and let the enemy stop there.
     }
 }
